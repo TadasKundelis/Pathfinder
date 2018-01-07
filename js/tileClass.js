10ff61707552f58639bd8b02dba4c976ef6696a3
TadasKundelis <kundelist@gmail.com>
2018-01-07 21:54:45 +0200

    Update tileClass.js

--- js/tileClass.js
+++ js/tileClass.js
@@ -8,7 +8,7 @@ export default class Tile {
     this.y = y; // Y coordinate pixelwise
     this.state = state; // empty, wall, start, finish or path
     this.row = row; //row in the matrix
-    this.col = col //col in the matrix
+    this.col = col; //col in the matrix
     this.available = true;
     this.inTheList = false;
     this.initialCost = 0; //property for Manhattan distance to the finish square(initialized as 0)
@@ -26,9 +26,9 @@ export default class Tile {
     this.initialCost = 0;
     this.accumulatedCost = 0;
     this.totalCost = undefined;
-    this.inTheList = false
-    this.available = true
-    this.parent = undefined
+    this.inTheList = false;
+    this.available = true;
+    this.parent = undefined;
     this.draw(this.state)
   }
 }
